5eb1e43ad4b44ed583ec6e95a98db81c192872c0
adm1009 <abhijeetmulik319@gmail.com>
2025-03-13 12:09:35 +0530

    test codepipeline

--- lambdas/createUser.js
+++ lambdas/createUser.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'User created successfully!' })
+        body: JSON.stringify({ message: 'a User created successfully!' })
     };
 };
